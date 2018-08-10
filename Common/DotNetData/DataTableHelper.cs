ecc06ab648ab778eec9884509d3ebb6c0bba5c44
johnsing <johnsing.he@flex.com>
2018-08-10 08:54:45 +0800

    backup

--- Common/DotNetData/DataTableHelper.cs
+++ Common/DotNetData/DataTableHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 
 namespace Common.DotNetData
 {
@@ -99,6 +101,40 @@ namespace Common.DotNetData
             DataTable dtNew = DataTableRotate(dt, dtVal);
             dtNew.Rows.RemoveAt(0);
             return dtNew;
+        }
+
+		//TODO 如果有部分不符合约束条件的话，会导致整个插入失败。
+        public static bool ImportDatatable(DbConnection conn, DataTable dt, string sTabName, out string serr)
+        {
+            serr = string.Empty;
+            bool bRet = false;
+            var sqlConn = conn as SqlConnection;
+            var tran = sqlConn.BeginTransaction();
+            try
+            {
+                using (var bulkCopy = new SqlBulkCopy(sqlConn, SqlBulkCopyOptions.Default, tran))
+                {
+                    bulkCopy.BatchSize = 1000;
+                    bulkCopy.DestinationTableName = sTabName;
+                    //if (columnMappings != null)
+                    //{
+                    //    foreach (var col in columnMappings)
+                    //    {
+                    //        bulkCopy.ColumnMappings.Add(col.Value, col.Key);
+                    //    }
+                    //}
+                    bulkCopy.WriteToServer(dt);
+                }
+                tran.Commit();
+                bRet = true;
+            }
+            catch (Exception ex)
+            {
+                tran.Rollback();
+                LogHelper.WriteError(conn.GetType(), ex);
+                serr = "部分课程编号有重复，无法导入";
+            }
+            return bRet;
         }
     }
 }
