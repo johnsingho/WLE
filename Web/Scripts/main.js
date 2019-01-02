9c2c79f5f774376dd18db2d8f08b0f55bf9401f0
johnsing <johnsing.he@flex.com>
2019-01-02 11:49:06 +0800

    add LogVisitTrack

--- Web/Scripts/main.js
+++ Web/Scripts/main.js
@@ -162,4 +162,25 @@ function MakeValueArr(obj) {
     return res;
 }
 
+/**
+* sysName -- 系统名字，必需
+* modName -- 模块名字，可选
+* loginUser -- 登录用户名，可选
+*/
+function VisitTrack(sysName, modName, loginUser) {
+    var sUrl = "http://dmnnt022:9900/VisitTrackService.svc/VisitTrackWeb/Visit";
+    var paras = {
+        'sysname': sysName,
+        'modname': modName,
+        'loginuser': loginUser
+    };
+    $.ajax({
+        url: sUrl,
+        data: paras,
+        dataType: "jsonp",
+        error: function (xhr, status, error) {
+            console.log("Error:" + error);
+        }
+    });
+}
 
